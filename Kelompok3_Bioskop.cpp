0371d50642c80cca86ad540d2716d5343830c2c5
Devina Agnesia Pratami <tamivina321@gmail.com>
2025-06-01 16:12:14 +0700

    Update Kelompok3_Bioskop.cpp

--- Kelompok3_Bioskop.cpp
+++ Kelompok3_Bioskop.cpp
@@ -11,6 +11,27 @@ struct Bioskop {
   string nama;
 }
 
+// Fungsi untuk login admin
+bool adminLogin() {
+    string username, password;
+    const string ADMIN_USERNAME = "admin";
+    const string ADMIN_PASSWORD = "password";
+
+    cout << "==== Login Admin ====" << endl;
+    cout << "Username: ";
+    cin >> username;
+    cout << "Password: ";
+    cin >> password;
+
+    if (username == ADMIN_USERNAME && password == ADMIN_PASSWORD) {
+        cout << "Login berhasil!\n" << endl;
+        return true;
+    } else {
+        cout << "Login gagal! Silakan coba lagi.\n" << endl;
+        return false;
+    }
+}
+
 void clearInputBuffer() {
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 }
