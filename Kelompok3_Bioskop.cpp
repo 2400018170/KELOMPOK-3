12b98b6a0125002264a71254958803f680b47b76
Devina Agnesia Pratami <tamivina321@gmail.com>
2025-06-01 17:20:36 +0700

    Update Kelompok3_Bioskop.cpp

--- Kelompok3_Bioskop.cpp
+++ Kelompok3_Bioskop.cpp
@@ -7,12 +7,46 @@
 #include <limits>  
 using namespace std;
 
-struct Menu {
-  string nama;
+void simpanDataPembeli() {
+    string nama, nowa;
+
+    cin.ignore();
+    cout << "=========== INPUT DATA PEMBELI ===========" << endl;
+    cout << "Masukkan Nama Pembeli       : ";
+    getline(cin, nama);
+
+    cout << "Masukkan Nomor WhatsApp     : ";
+    getline(cin, nowa);
+
+    ofstream file("datapembeliantiket.txt", ios::app);
+    if (!file.is_open()) {
+        cerr << "? Gagal membuka file untuk ditulis." << endl;
+        return;
+    }
+
+    file << "Nama Pembeli     : " << nama << endl;
+    file << "Nomor WhatsApp   : " << nowa << endl;
+    file << "------------------------------------------\n";
+
+    file.close();
+    cout << "\n Data berhasil disimpan ke file.\n\n";
 }
 
-struct Film {
-  string nama;
+// Fungsi untuk menampilkan semua data pembeli dari file
+void tampilkanDataPembeli() {
+    ifstream file("datapembeliantiket.txt");
+    if (!file.is_open()) {
+        cerr << "? Gagal membuka file untuk dibaca." << endl;
+        return;
+    }
+
+    cout << "=========== DAFTAR DATA PEMBELI ===========" << endl;
+    string line;
+    while (getline(file, line)) {
+        cout << line << endl;
+    }
+    file.close();
+    cout << "===========================================\n\n";
 }
 
 // Fungsi untuk login admin
@@ -53,6 +87,31 @@ int main (){
     while (!isLoggedIn) {
         isLoggedIn = adminlogin();
     }
+
+  char ulang;
+    do{
+        int opsi;
+        cout <<"========DAFTAR MENU========" <<endl;
+        cout <<" 1. Tambah Pembeli Ke Antrian "<<endl;
+        cout <<" 2. Daftar FILM "<<endl;
+        cout <<" 3. Pemesanan Tiket "<<endl;
+        cout <<" 4. Chekout "<<endl;
+        cout <<" 5. Cetak Tiket "<<endl;
+        cout <<" 6. Keluar "<<endl;
+        
+        cout<<"Masukkan pilihan anda : "; cin>>opsi;
+        
+        if(opsi==1){
+            char ulang;
+            do{
+                simpanDataPembeli();
+                cout<<"Tambah pembeli lagi? (y/n) : "; cin>>ulang;
+                cin.ignore();
+            }while(ulang=='y' || ulang=='Y');
+        }
+        cout << "Apakah Anda ingin mengulangi program Utama? (y/n): ";
+        cin >> ulang;
+    }while (ulang == 'y' || ulang == 'Y');
     
     return 0;
 }
