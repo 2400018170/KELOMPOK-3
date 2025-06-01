12d59ea2d3313f5d00b8c7b08fe503f1478c669b
2400018169 <alifiahartisya106@gmail.com>
2025-06-01 15:23:58 +0700

    Add files via upload

---
+++ Kelompok3_Bioskop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <sstream> // Untuk menggantikan to_string
+#include <limits>  // Untuk validasi input
+using namespace std;
+
+
